927bdd9e31b73e79e1976cdc2762cd9ffd09acb1
Aftab MJ <Aftab.Jalal@directionshealth.com>
2020-06-27 14:34:41 +1000

    minor change to test pipeline

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App, MJ"/>
+    <HelloWorld msg="Welcome to Your Vue.js App, MJ - haha"/>
     Hi
   </div>
 </template>
